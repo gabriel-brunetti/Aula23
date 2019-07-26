d29710780d9a3049b81229c2eeb035c1ca64d7d0
Gabriel Brunetti <gabriel.brunetti1@gmail.com>
2019-07-26 12:14:40 -0300

    Add files via upload

---
+++ CadastroDeFuncionarios/inc/functions.php
@@ -0,0 +1,77 @@
+<?php
+	// Definindo uma constante para o nome do arquivo
+	define('ARQUIVO','funcionarios.json');
+
+	// Função para validar dados do post
+	function errosNoPost(){
+		$erros =[];
+		if(!isset($_POST['nome']) || $_POST['nome']==''){
+			$erros[] = 'errNome';
+		}
+
+		if(!isset($_POST['email']) || $_POST['email']==''){
+			$erros[] = 'errEmail';
+		}
+
+		if(!isset($_POST['senha']) || $_POST['senha']==''){
+			$erros[] = 'errSenha';
+		}
+
+		if($_POST['conf'] != $_POST['senha'] && isset($_POST['conf'])){
+			$erros[] = 'errConf';
+		}
+
+		return $erros;
+	}
+
+	// Carregando o conteúdo do arquivo (string json) para uma variável
+	function getFuncionarios(){
+		$json = file_get_contents(ARQUIVO);
+		$funcionarios = json_decode($json,true);
+		return $funcionarios;
+	}
+	
+	// Função que adiciona funcionario ao json
+	function addFuncionario($nome,$email,$senha){
+
+		// Carregando os funcionarios
+		$funcionarios = getFuncionarios();
+
+		// Adicionando um novo funcionario ao array de funcionarios
+		$funcionarios[] = [
+			'nome' => $nome,
+			'email' => $email,
+			'senha' => password_hash($senha,PASSWORD_DEFAULT)
+			// função password_hash garante a cripotagria dos dados
+		];
+		
+		// Transformando o array funcionarios numa string json
+		$json = json_encode($funcionarios);
+
+		// Salvar a string json no arquivo
+		file_put_contents(ARQUIVO,$json); 
+	}
+
+	// Função para verificar se o login é válido
+	function logar($email,$senha){
+		
+		// Carregar o Json
+		$funcionarios = getFuncionarios();
+
+		// Procurar o funcionário com o e-mail dado
+		$achou = false;
+		foreach ($funcionarios as $f) {
+			if($f['email'] == $email){
+				$achou = true;
+				break;
+				// o break interrompe o forearch/LOOP; não interrompe o script
+			}
+		}
+
+		if(!$achou){
+			return false;
+		} else {
+			$senhaOk = password_verify($senha,$f['senha']);
+			return $senhaOk;
+		}
+	}
